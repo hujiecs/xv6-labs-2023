8a445914077e073f29be8fa244a20f330b91a85b
hujie <hujieqwert+apple@gmail.com>
2024-03-14 22:11:40 +0800

    Print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,29 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprintinternal(pagetable_t pagetable, int depth)
+{
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if ((pte & PTE_V) == 0)
+      continue;
+    
+    for (int j = 0; j <= depth; j++)
+      printf("..");
+    
+    uint64 pa = PTE2PA(pte);
+    printf("%d: pte %p pa %p\n", i, pte, pa);
+
+    // point to low level page table so don't have RWX set.
+    if ((pte & (PTE_R | PTE_W | PTE_X)) == 0)
+      vmprintinternal((pagetable_t)pa, depth + 1);
+  }
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  vmprintinternal(pagetable, 0);
+}
