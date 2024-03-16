21b36de19d956cd50b8f03f29d49a80dde43ccf2
hujie <hujieqwert+apple@gmail.com>
2024-03-16 14:50:02 +0800

    Implement copy-on-write fork

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -23,6 +23,8 @@ struct {
   struct run *freelist;
 } kmem;
 
+int refcount[PHYSTOP / PGSIZE] = {0};
+
 void
 kinit()
 {
@@ -35,8 +37,10 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE) {
+    refcount[(uint64)p / PGSIZE] = 1;
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by pa,
@@ -50,6 +54,15 @@ kfree(void *pa)
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
+
+  acquire(&kmem.lock);
+  int cnt = --refcount[(uint64)pa / PGSIZE];
+  if (cnt < 0)
+    panic("kfree: negative refcont");
+  release(&kmem.lock);
+
+  if (cnt > 0)
+    return;
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
@@ -72,11 +85,21 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if(r) {
     kmem.freelist = r->next;
+    refcount[(uint64)r / PGSIZE] = 1;
+  }
   release(&kmem.lock);
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
+}
+
+void
+addref(void *pa)
+{
+  acquire(&kmem.lock);
+  refcount[(uint64)pa / PGSIZE]++;
+  release(&kmem.lock);
 }
--- kernel/vm.c
+++ kernel/vm.c
@@ -314,8 +314,6 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
 {
   pte_t *pte;
   uint64 pa, i;
-  uint flags;
-  char *mem;
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
@@ -323,14 +321,16 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
     if((*pte & PTE_V) == 0)
       panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
-    flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto err;
-    memmove(mem, (char*)pa, PGSIZE);
-    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-      kfree(mem);
-      goto err;
+
+    // clear PTE_W for both parent and child, 
+    // if don't clear parent, parent modified
+    // page, child will copy dirty page
+    if (*pte & PTE_W) {
+      *pte = (*pte & ~PTE_W) | PTE_C;
     }
+    if(mappages(new, i, PGSIZE, (uint64)pa, PTE_FLAGS(*pte)) != 0)
+      goto err;
+    addref((void*)pa);
   }
   return 0;
 
@@ -367,7 +367,9 @@ copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
       return -1;
     pte = walk(pagetable, va0, 0);
     if(pte == 0 || (*pte & PTE_V) == 0 || (*pte & PTE_U) == 0 ||
-       (*pte & PTE_W) == 0)
+       ((*pte & PTE_W) == 0 && !(*pte & PTE_C)))
+      return -1;
+    if ((*pte & PTE_C) && cowfault(pagetable, va0) < 0)
       return -1;
     pa0 = PTE2PA(*pte);
     n = PGSIZE - (dstva - va0);
@@ -448,4 +450,29 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
   } else {
     return -1;
   }
+} 
+
+int
+cowfault(pagetable_t pagetable, uint64 va)
+{
+  if (va >= MAXVA) // needed to pass MAXVAplus test
+    return -1;
+  pte_t *pte  = walk(pagetable, va, 0);
+  if (pte == 0)
+    return -1;
+  
+  if (!(*pte & PTE_U) || !(*pte & PTE_V) || !(*pte & PTE_C))
+    return -1;
+  
+  uint64 pa  = PTE2PA(*pte);
+  char *mem = kalloc();
+  if (mem == 0)
+    return -1;
+  memmove((void*)mem, (void*)pa, PGSIZE);
+  int flags = (PTE_FLAGS(*pte) | PTE_W) & ~PTE_C;
+  uvmunmap(pagetable, PGROUNDDOWN(va), 1, 1);
+  if (mappages(pagetable, PGROUNDDOWN(va), PGSIZE, (uint64)mem, flags) < 0)
+    return -1;
+
+  return 0;
 }
